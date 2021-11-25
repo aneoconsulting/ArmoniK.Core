45e91b2283a4d5972b26b193c0a446163c74171f
Jérôme Gurhem <jgurhem@aneo.fr>
2021-11-25 20:01:26 +0100

    Initialize TableStorage in Startup

--- src/Control/src/ArmoniK.Control.Service/ArmoniK.Control/Startup.cs
+++ src/Control/src/ArmoniK.Control.Service/ArmoniK.Control/Startup.cs
@@ -10,6 +10,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using ArmoniK.Control.Services;
+using ArmoniK.Core.Storage;
+using ArmoniK.Adapters.MongoDB;
+using ArmoniK.Core.Exceptions;
+using MongoDB.Driver;
 
 namespace ArmoniK.Control
 {
@@ -20,6 +24,42 @@ namespace ArmoniK.Control
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddSingleton<ITableStorage>(sp =>
+                {
+                    string db_type = Environment.GetEnvironmentVariable("TASKS_STATUS_TABLE_SERVICE");
+                    if (db_type == null)
+                    {
+                        throw new ArmoniKException("env var TASKS_STATUS_TABLE_SERVICE not found ");
+                    }
+                    if (!string.Equals(db_type, "MongoDB"))
+                    {
+                        throw new ArmoniKException("Database type not supported (" + db_type + ")");
+                    }
+                    string db_endpoint = Environment.GetEnvironmentVariable("DB_ENDPOINT");
+                    if (db_endpoint == null)
+                    {
+                        throw new ArmoniKException("env var DB_ENDPOINT not found ");
+                    }
+                    string db_port = Environment.GetEnvironmentVariable("DB_PORT");
+                    if (db_port == null)
+                    {
+                        throw new ArmoniKException("env var DB_PORT not found ");
+                    }
+                    string db_table = Environment.GetEnvironmentVariable("TASKS_STATUS_TABLE_NAME");
+                    if (db_table == null)
+                    {
+                        throw new ArmoniKException("env var TASKS_STATUS_TABLE_NAME not found ");
+                    }
+                    MongoClient mongoClient = new MongoClient("mongodb://" + db_endpoint + ":" + db_port);
+                    IMongoDatabase mongoDatabase = mongoClient.GetDatabase(db_table);
+                    mongoDatabase.CreateCollection("session", new CreateCollectionOptions<SessionDataModel>());
+                    mongoDatabase.CreateCollection("task", new CreateCollectionOptions<TaskDataModel>());
+                    IMongoCollection<SessionDataModel> sessionCollection = mongoDatabase.GetCollection<SessionDataModel>("session");
+                    IMongoCollection<TaskDataModel> taskCollection = mongoDatabase.GetCollection<TaskDataModel>("task");
+                    TimeSpan pollingDelay = new TimeSpan(seconds: 5, minutes: 0, hours: 0, days: 0);
+                    return new TableStorage(sessionCollection, taskCollection, mongoClient.StartSession(), pollingDelay);
+                });
+            services.AddSingleton<ClientService, ClientService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
