56643b49f1cc41e0d61b975feb0252f3184513e5
Jérôme Gurhem <jgurhem@aneo.fr>
2021-12-09 14:54:17 +0100

    Rework Heart class

---
+++ src/Core/src/Utils/AsyncLazy.cs
@@ -0,0 +1,26 @@
+﻿// This file is part of ArmoniK project.
+// 
+// Copyright (c) ANEO. All rights reserved.
+//   W. Kirschenmann <wkirschenmann@aneo.fr>
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ArmoniK.Core.Utils
+{
+  public class AsyncLazy<T> : Lazy<Task<T>>
+  {
+    public AsyncLazy(Func<T> valueFactory) :
+      base(() => Task.FromResult(valueFactory()))
+    {
+    }
+
+    public AsyncLazy(Func<Task<T>> taskFactory) :
+      base(taskFactory)
+    {
+    }
+
+    public TaskAwaiter<T> GetAwaiter() => Value.GetAwaiter();
+  }
+}
--- src/Core/src/Utils/Heart.cs
+++ src/Core/src/Utils/Heart.cs
@@ -1,31 +1,11 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ArmoniK.Core.Utils
 {
-  public class AsyncLazy<T> : Lazy<Task<T>>
-  {
-    public AsyncLazy(Func<T> valueFactory) :
-      base(() => Task.FromResult(valueFactory()))
-    {
-    }
-
-    public AsyncLazy(Func<Task<T>> taskFactory) :
-      base(taskFactory)
-    {
-    }
-
-    public TaskAwaiter<T> GetAwaiter() => Value.GetAwaiter();
-  }
-
   public class Heart
   {
-    private bool IsStarted => nbPulsations_ >= 0;
-
-    private int nbPulsations_ = -1;
-
     private readonly CancellationToken cancellationToken_;
 
     private CancellationTokenSource stoppedHeartCts_ = new();
@@ -36,7 +16,7 @@ namespace ArmoniK.Core.Utils
 
     private readonly Func<CancellationToken, Task<bool>> pulse_;
 
-    private readonly TimeSpan BeatPeriod;
+    private readonly TimeSpan beatPeriod;
 
     /// <summary>
     /// 
@@ -53,7 +33,7 @@ namespace ArmoniK.Core.Utils
     {
       cancellationToken_ = cancellationToken;
       pulse_             = pulse;
-      BeatPeriod         = beatPeriod;
+      this.beatPeriod    = beatPeriod;
       stoppedHeartCts_.Cancel();
     }
 
@@ -79,7 +59,6 @@ namespace ArmoniK.Core.Utils
     /// <returns>A task finishing with the last heartbeat</returns>
     public Task Stop()
     {
-      nbPulsations_ = 2;
       stoppedHeartCts_.Cancel();
       return runningTask_;
     }
@@ -87,41 +66,23 @@ namespace ArmoniK.Core.Utils
     /// <summary>
     /// Start the heart. If the heart is beating, it has no effect.
     /// </summary>
-    /// <param name="nbPulsations">Number of cycles to do. Set 0 for an infinite loop.</param>
-    public void Start(int nbPulsations = 0)
+    public void Start()
     {
-      if (nbPulsations_ == 0) // already running with infinite loop
+      if (!stoppedHeartCts_.IsCancellationRequested) // already running with infinite loop
       {
-        return;
-      }
-
-
-      if (nbPulsations_ > 0) // already running
-      {
-        nbPulsations_ = Math.Max(nbPulsations_,
-                                 nbPulsations);
         return;
       }
 
       stoppedHeartCts_ = new CancellationTokenSource();
-      nbPulsations_    = nbPulsations;
       combinedSource_ = CancellationTokenSource.CreateLinkedTokenSource(stoppedHeartCts_.Token,
                                                                         cancellationToken_);
 
-      runningTask_ = Task.Factory
+      runningTask_ = Task<Task>.Factory
                          .StartNew(async () =>
                                    {
-                                     while (IsStarted)
+                                     while (!stoppedHeartCts_.IsCancellationRequested)
                                      {
-                                       NextPulseWaiter = FullCycle();
-                                       await NextPulseWaiter;
-                                       if (nbPulsations_ == 1)
-                                       {
-                                         nbPulsations_ = -1;
-                                         stoppedHeartCts_.Cancel();
-                                       }
-
-                                       if (nbPulsations_ > 1) --nbPulsations_;
+                                       await FullCycle();
                                      }
                                    },
                                    cancellationToken_,
@@ -131,23 +92,17 @@ namespace ArmoniK.Core.Utils
     }
 
     /// <summary>
-    /// A task that finished with the next pulse
-    /// </summary>
-    public Task NextPulseWaiter { get; private set; }
-
-    /// <summary>
     /// Triggered when the heart stops
     /// </summary>
     public CancellationToken HeartStopped => stoppedHeartCts_.Token;
 
     private async Task FullCycle()
     {
-      var delayTask = Task.Delay(BeatPeriod,
+      var delayTask = Task.Delay(beatPeriod,
                                  combinedSource_.Token);
       if (!await pulse_(cancellationToken_))
       {
         stoppedHeartCts_.Cancel();
-        nbPulsations_ = -1;
         return;
       }
 
