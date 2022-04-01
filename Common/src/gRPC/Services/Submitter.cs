9b4d4f7696af939f5dd138c419eac0769e61f2fa
Jérôme Gurhem <jgurhem@aneo.fr>
2022-04-01 21:04:13 +0200

    Remove unnecessary usings

--- Common/src/gRPC/Services/Submitter.cs
+++ Common/src/gRPC/Services/Submitter.cs
@@ -25,9 +25,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
 using System.Threading.Tasks;
 
