719dfb59cc087eb827c8f5491543fd5516aaeb36
Jérôme Gurhem <jgurhem@aneo.fr>
2022-04-03 00:31:32 +0200

    Some cleaning

--- Common/src/Storage/QueueMessageHandler.cs
+++ Common/src/Storage/QueueMessageHandler.cs
@@ -25,8 +25,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-using JetBrains.Annotations;
-
 using Microsoft.Extensions.Logging;
 
 namespace ArmoniK.Core.Common.Storage;
