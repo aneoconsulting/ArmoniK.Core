8d6adaa1f70652a1e4d45eb254aa507e3ea58fb9
Jérôme Gurhem <jgurhem@aneo.fr>
2022-05-19 19:45:59 +0200

    Add missing class

---
+++ Common/src/Injection/Options/InitWorker.cs
@@ -0,0 +1,35 @@
+﻿// This file is part of the ArmoniK project
+// 
+// Copyright (C) ANEO, 2021-2022. All rights reserved.
+//   W. Kirschenmann   <wkirschenmann@aneo.fr>
+//   J. Gurhem         <jgurhem@aneo.fr>
+//   D. Dubuc          <ddubuc@aneo.fr>
+//   L. Ziane Khodja   <lzianekhodja@aneo.fr>
+//   F. Lemaitre       <flemaitre@aneo.fr>
+//   S. Djebbar        <sdjebbar@aneo.fr>
+//   J. Fonseca        <jfonseca@aneo.fr>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace ArmoniK.Core.Common.Injection.Options;
+
+public class InitWorker
+{
+  public const string SettingSection = nameof(InitWorker);
+
+  public int      WorkerCheckRetries { get; set; } = 10;
+  public TimeSpan WorkerCheckDelay   { get; set; } = TimeSpan.FromSeconds(30);
+}
