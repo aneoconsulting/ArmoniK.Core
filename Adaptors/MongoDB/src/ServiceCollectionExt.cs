aba08b47a2e2e150e01a7eb20893160dde41728b
Jérôme Gurhem <jgurhem@aneo.fr>
2022-06-02 20:14:03 +0200

    Put parameter for MaxConnectionPoolSize

--- Adaptors/MongoDB/src/ServiceCollectionExt.cs
+++ Adaptors/MongoDB/src/ServiceCollectionExt.cs
@@ -194,30 +194,21 @@ public static class ServiceCollectionExt
     }
 
     var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
-    settings.AllowInsecureTls = mongoOptions.AllowInsecureTls;
-    settings.UseTls           = mongoOptions.Tls;
-    settings.DirectConnection = mongoOptions.DirectConnection;
-    settings.Scheme           = ConnectionStringScheme.MongoDB;
+    settings.AllowInsecureTls      = mongoOptions.AllowInsecureTls;
+    settings.UseTls                = mongoOptions.Tls;
+    settings.DirectConnection      = mongoOptions.DirectConnection;
+    settings.Scheme                = ConnectionStringScheme.MongoDB;
+    settings.MaxConnectionPoolSize = mongoOptions.MaxConnectionPoolSize;
 
     services.AddTransient<IMongoClient>(provider =>
                                         {
-                                          //if (logger.IsEnabled(LogLevel.Trace))
-                                          //  settings.ClusterConfigurator = cb =>
-                                          //  {
-                                          //    cb.Subscribe<CommandStartedEvent>(e =>
-                                          //    {
-                                          //      logger
-                                          //        .LogTrace("{CommandName} - {Command}",
-                                          //                  e.CommandName,
-                                          //                  e.Command.ToJson());
-                                          //    });
-                                          //  };
-
-                                          settings.ClusterConfigurator += cb => cb.Subscribe<CommandStartedEvent>(e => logger.LogInformation("{CommandName} - {Command}",
-                                                                                                                                            e.CommandName,
-                                                                                                                                            e.Command.ToJson()));
-
-                                          settings.ClusterConfigurator += cb => cb.Subscribe(new DiagnosticsActivityEventSubscriber());
+                                          settings.ClusterConfigurator += cb =>
+                                                                          {
+                                                                            cb.Subscribe<CommandStartedEvent>(e => logger.LogInformation("{CommandName} - {Command}",
+                                                                                                                                         e.CommandName,
+                                                                                                                                         e.Command.ToJson()));
+                                                                            cb.Subscribe(new DiagnosticsActivityEventSubscriber());
+                                                                          };
 
                                           return new MongoClient(settings);
                                         });
