91f60bd0cdff285f903ffb6d6829bd20f1fe23c5
Jérôme Gurhem <jgurhem@aneo.fr>
2022-07-21 15:42:19 +0200

    Fix editorconfig

--- Common/src/Storage/LockedWrapperQueueStorage.cs
+++ Common/src/Storage/LockedWrapperQueueStorage.cs
@@ -15,7 +15,12 @@
 // (at your option) any later version.
 // 
 // This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
