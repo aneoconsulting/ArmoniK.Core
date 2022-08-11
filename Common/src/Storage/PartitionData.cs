b04b9922f6962715bbeb72bc91df6679018bd0d4
Jérôme Gurhem <jgurhem@aneo.fr>
2022-08-11 15:02:11 +0200

    Add doc for partition data

--- Common/src/Storage/PartitionData.cs
+++ Common/src/Storage/PartitionData.cs
@@ -26,6 +26,16 @@ using System.Collections.Generic;
 
 namespace ArmoniK.Core.Common.Storage;
 
+/// <summary>
+///   Data structure to hold partitions metadata
+/// </summary>
+/// <param name="PartitionId">Unique name of the partition</param>
+/// <param name="ParentPartitionIds">List of parents up to the first partition</param>
+/// <param name="PodReserved">Number of reserved pods</param>
+/// <param name="PodMax">Max number of pods</param>
+/// <param name="PreemptionPercentage">Percentage of pods that can be preempted</param>
+/// <param name="Priority">Priority of the partition</param>
+/// <param name="PodConfiguration">Pod configuration used to select machines</param>
 public record PartitionData(string           PartitionId,
                             IList<string>    ParentPartitionIds,
                             int              PodReserved,
