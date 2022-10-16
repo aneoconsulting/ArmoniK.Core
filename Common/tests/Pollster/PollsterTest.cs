6e0c20034afe8a6da223353d99f49ede8dd36d67
José Fonseca <jfonseca@aneo.fr>
2022-10-16 14:14:09 +0200

    restore test

--- Common/tests/Pollster/PollsterTest.cs
+++ Common/tests/Pollster/PollsterTest.cs
@@ -544,7 +544,6 @@ public class PollsterTest
   }
 
   [Test]
-  [Ignore("To fix in next PR")]
   [TestCaseSource(nameof(ExecuteTooManyErrorShouldFailTestCase))]
   public async Task ExecuteTooManyErrorShouldFail((Mock<IWorkerStreamHandler>, Mock<IPullQueueStorage>, Mock<IAgentHandler>) mocks)
   {
