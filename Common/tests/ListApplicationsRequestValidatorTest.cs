6c79826c0f5fece3ce1f335869fbc6bfa415183e
Jérôme Gurhem <jgurhem@aneo.fr>
2022-11-09 07:56:08 +0100

    Fix test name

--- Common/tests/ListApplicationsRequestValidatorTest.cs
+++ Common/tests/ListApplicationsRequestValidatorTest.cs
@@ -136,7 +136,7 @@ public class ListApplicationsRequestValidatorTest
   }
 
   [Test]
-  public void ListApplicationsZeroNegativePageSizeShouldFail()
+  public void ListApplicationsRequestZeroPageSizeShouldFail()
   {
     validListApplicationsRequest_!.PageSize = 0;
     Assert.IsFalse(validator_.Validate(validListApplicationsRequest_)
