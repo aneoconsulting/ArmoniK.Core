9a07b3e0e1594c11f875625e6806d9eae9cf764f
José Fonseca <jfonseca@aneo.fr>
2023-01-09 12:02:09 +0100

    avoid conditional with terraform's one function

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,9 @@
 locals {
   replicas        = toset([for s in range(var.num_replicas) : tostring(s)])
-  queue_env_vars  = var.queue_storage.broker.name == "activemq" ? module.queue_activemq[0].queue_env_vars : module.queue_rabbitmq[0].queue_env_vars
-  object_env_vars = var.object_storage.name == "redis" ? module.object_redis[0].object_env_vars : module.object_local[0].object_env_vars
+  queue           = one(concat(module.queue_activemq, module.queue_rabbitmq))
+  queue_env_vars  = local.queue.queue_env_vars
+  object          = one(concat(module.object_redis, module.object_local))
+  object_env_vars = local.object.object_env_vars
   submitter       = merge(var.submitter, { tag = var.core_tag })
   compute_plane   = merge(var.compute_plane, { tag = var.core_tag })
 }
