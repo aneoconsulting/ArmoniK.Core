9036de43b5e118ff9e645f75d4b277283c778f6b
Jérôme Gurhem <jgurhem@aneo.fr>
2023-02-24 18:30:45 +0100

    Fix Dependency Checker service startup

--- Control/DependencyChecker/src/Program.cs
+++ Control/DependencyChecker/src/Program.cs
@@ -1,24 +1,17 @@
 // This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2021-2022. All rights reserved.
-//   W. Kirschenmann   <wkirschenmann@aneo.fr>
-//   J. Gurhem         <jgurhem@aneo.fr>
-//   D. Dubuc          <ddubuc@aneo.fr>
-//   L. Ziane Khodja   <lzianekhodja@aneo.fr>
-//   F. Lemaitre       <flemaitre@aneo.fr>
-//   S. Djebbar        <sdjebbar@aneo.fr>
-//   J. Fonseca        <jfonseca@aneo.fr>
-//
+// 
+// Copyright (C) ANEO, 2021-2023. All rights reserved.
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -119,11 +112,7 @@ public static class Program
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseAuthentication();
-
       app.UseRouting();
-
-      app.UseAuthorization();
       app.UseSerilogRequestLogging();
 
       app.UseHealthChecks("/startup",
@@ -140,19 +129,16 @@ public static class Program
                             Predicate = check => check.Tags.Contains(nameof(HealthCheckTag.Liveness)),
                           });
 
-
-      if (app.Environment.IsDevelopment())
-      {
-        app.MapGrpcReflectionService();
-      }
-
       var sessionProvider      = app.Services.GetRequiredService<SessionProvider>();
       var pushQueueStorage     = app.Services.GetRequiredService<IPushQueueStorage>();
+      var pullQueueStorage     = app.Services.GetRequiredService<IPullQueueStorage>();
       var taskPushQueueStorage = pushQueueStorage.Init(CancellationToken.None);
+      var taskPullQueueStorage = pullQueueStorage.Init(CancellationToken.None);
 
       await sessionProvider.Init(CancellationToken.None)
                            .ConfigureAwait(false);
       await taskPushQueueStorage.ConfigureAwait(false);
+      await taskPullQueueStorage.ConfigureAwait(false);
 
       await app.RunAsync()
                .ConfigureAwait(false);
