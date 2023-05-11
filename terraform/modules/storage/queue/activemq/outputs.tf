59cc82a0a2c5dcbfb57bfb1ec527a78a7e321101
Nicolas GRUEL (ANEO) <ngruel@aneo.fr>
2023-05-11 22:34:39 +0200

    Revert change made for the queue adapter to be used as a plugin

--- terraform/modules/storage/queue/activemq/outputs.tf
+++ terraform/modules/storage/queue/activemq/outputs.tf
@@ -1,14 +1,13 @@
 output "generated_env_vars" {
   value = ({
-    "Components__QueueAdaptorSettings__ClassName"           = "ArmoniK.Core.Adapters.Amqp.QueueBuilder"
-    "Components__QueueAdaptorSettings__AdapterAbsolutePath" = "/adapters/queue/amqp/ArmoniK.Core.Adapters.Amqp.dll"
-    "Amqp__User"                                            = "${var.queue_envs.user}"
-    "Amqp__Password"                                        = "${var.queue_envs.password}"
-    "Amqp__Host"                                            = "${var.queue_envs.host}"
-    "Amqp__Port"                                            = "${var.queue_envs.port}"
-    "Amqp__Scheme"                                          = "AMQP"
-    "Amqp__MaxPriority"                                     = "${var.queue_envs.max_priority}"
-    "Amqp__MaxRetries"                                      = "${var.queue_envs.max_retries}"
-    "Amqp__LinkCredit"                                      = "${var.queue_envs.link_credit}"
+    "Components__QueueStorage" = "ArmoniK.Adapters.Amqp.QueueStorage"
+    "Amqp__User"               = "${var.queue_envs.user}"
+    "Amqp__Password"           = "${var.queue_envs.password}"
+    "Amqp__Host"               = "${var.queue_envs.host}"
+    "Amqp__Port"               = "${var.queue_envs.port}"
+    "Amqp__Scheme"             = "AMQP"
+    "Amqp__MaxPriority"        = "${var.queue_envs.max_priority}"
+    "Amqp__MaxRetries"         = "${var.queue_envs.max_retries}"
+    "Amqp__LinkCredit"         = "${var.queue_envs.link_credit}"
   })
 }
--- terraform/modules/storage/queue/rabbitmq/outputs.tf
+++ terraform/modules/storage/queue/rabbitmq/outputs.tf
@@ -1,15 +1,14 @@
 output "generated_env_vars" {
   value = ({
     # RabbitMQ adapter targets version 0.9.1 of the protocol, if the version 1.0.0 is to be used the AMQP adapter should be employed
-    "Components__QueueAdaptorSettings__ClassName"           = var.protocol == "amqp1_0" ? "ArmoniK.Core.Adapters.Amqp.QueueBuilder" : "ArmoniK.Core.Adapters.RabbitMQ.QueueBuilder"
-    "Components__QueueAdaptorSettings__AdapterAbsolutePath" = var.protocol == "amqp1_0" ? "/adapters/queue/amqp/ArmoniK.Core.Adapters.Amqp.dll" : "/adapters/queue/rabbit/ArmoniK.Core.Adapters.RabbitMQ.dll"
-    "Amqp__User"                                            = "guest" # Default value, to change it we should provide a suitable .conf file
-    "Amqp__Password"                                        = "guest" # Default value, to change it we should provide a suitable .conf file
-    "Amqp__Host"                                            = "${var.queue_envs.host}"
-    "Amqp__Port"                                            = "${var.queue_envs.port}"
-    "Amqp__Scheme"                                          = "AMQP"
-    "Amqp__MaxPriority"                                     = "${var.queue_envs.max_priority}"
-    "Amqp__MaxRetries"                                      = "${var.queue_envs.max_retries}"
-    "Amqp__LinkCredit"                                      = "${var.queue_envs.link_credit}"
+    "Components__QueueStorage" = var.protocol == "amqp1_0" ? "ArmoniK.Adapters.Amqp.QueueStorage" : "ArmoniK.Adapters.RabbitMQ.QueueStorage"
+    "Amqp__User"               = "guest" # Default value, to change it we should provide a suitable .conf file
+    "Amqp__Password"           = "guest" # Default value, to change it we should provide a suitable .conf file
+    "Amqp__Host"               = "${var.queue_envs.host}"
+    "Amqp__Port"               = "${var.queue_envs.port}"
+    "Amqp__Scheme"             = "AMQP"
+    "Amqp__MaxPriority"        = "${var.queue_envs.max_priority}"
+    "Amqp__MaxRetries"         = "${var.queue_envs.max_retries}"
+    "Amqp__LinkCredit"         = "${var.queue_envs.link_credit}"
   })
 }
