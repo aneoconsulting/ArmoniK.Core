f7ae367bbabdcbd72c0d968d2c96661f6a00518f
melflitty <melflitty@aneo.fr>
2023-08-10 09:59:26 +0200

    fix: depending on the chunk download size this payload might not be a single async but more

--- Common/tests/TestBase/ObjectStorageTestBase.cs
+++ Common/tests/TestBase/ObjectStorageTestBase.cs
@@ -169,10 +169,15 @@ public class ObjectStorageTestBase
   {
     if (RunTests)
     {
-      var res = ObjectStorage!.GetValuesAsync("dataKey2");
-      var data = await res.SingleAsync()
-                          .ConfigureAwait(false);
-      var str = Encoding.ASCII.GetString(data);
+      var res  = ObjectStorage!.GetValuesAsync("dataKey2");
+      var data = new List<byte>();
+      foreach (var item in await res.ToListAsync()
+                                    .ConfigureAwait(false))
+      {
+        data.AddRange(item);
+      }
+
+      var str = Encoding.ASCII.GetString(data.ToArray());
       Console.WriteLine(str);
       Assert.IsTrue(str.SequenceEqual("AAAABBBB"));
     }
