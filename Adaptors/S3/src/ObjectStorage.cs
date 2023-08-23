5028450cb9044a854319455d76fd4ed920657848
melflitty <melflitty@aneo.fr>
2023-08-23 18:09:33 +0200

    refactor: remove multipart helper to object storage class

--- Adaptors/S3/src/ObjectStorage.cs
+++ Adaptors/S3/src/ObjectStorage.cs
@@ -34,6 +34,9 @@ namespace ArmoniK.Core.Adapters.S3;
 
 public class ObjectStorage : IObjectStorage
 {
+  private const    long                   MinPartSize  = 5 * 1024 * 1024; // 5 MiB
+  private const    int                    MaxPartCount = 10000;
+  private const    long                   MaxPartSize  = 5L * 1024 * 1024 * 1024; // 5 GiB
   private readonly string                 bucketName_;
   private readonly int                    chunkDownloadSize_;
   private readonly int                    degreeOfParallelism_;
@@ -205,11 +208,11 @@ public class ObjectStorage : IObjectStorage
                                       .ConfigureAwait(false);
     try
     {
-      var uploadRequest = UploadMultiPartHelper.PreparePartRequestsAsync(bucketName_,
-                                                                         objectStorageFullName,
-                                                                         initResponse.UploadId,
-                                                                         valueChunks,
-                                                                         cancellationToken);
+      var uploadRequest = PreparePartRequestsAsync(bucketName_,
+                                                   objectStorageFullName,
+                                                   initResponse.UploadId,
+                                                   valueChunks,
+                                                   cancellationToken);
       var uploadResponses = await uploadRequest.ParallelSelect(new ParallelTaskOptions(degreeOfParallelism_),
                                                                async uploadPartRequest => await s3Client_.UploadPartAsync(uploadPartRequest,
                                                                                                                           cancellationToken)
@@ -241,6 +244,88 @@ public class ObjectStorage : IObjectStorage
       await s3Client_.AbortMultipartUploadAsync(abortMpuRequest,
                                                 cancellationToken);
       throw;
+    }
+  }
+
+  private static async IAsyncEnumerable<UploadPartRequest> PreparePartRequestsAsync(string                                     bucketName,
+                                                                                    string                                     objectKey,
+                                                                                    string                                     uploadId,
+                                                                                    IAsyncEnumerable<ReadOnlyMemory<byte>>     valueChunks,
+                                                                                    [EnumeratorCancellation] CancellationToken cancellationToken)
+  {
+    var  partNumber        = 1;
+    long bytesRead         = 0;
+    var  currentPartSize   = MinPartSize;
+    var  currentPartStream = new MemoryStream();
+
+    await foreach (var chunk in valueChunks.WithCancellation(cancellationToken)
+                                           .ConfigureAwait(false))
+    {
+      long chunkSize = chunk.Length;
+      bytesRead += chunkSize;
+      var remainingStream = new MemoryStream();
+
+      if (bytesRead <= MaxPartSize)
+      {
+        await currentPartStream.WriteAsync(chunk.ToArray(),
+                                           cancellationToken)
+                               .ConfigureAwait(false);
+      }
+      else
+      {
+        var plusSize = bytesRead - MaxPartSize;
+        var chunkSlice = chunk.Slice(0,
+                                     (int)(chunkSize - plusSize));
+        var remainingSlice = chunk.Slice((int)(chunkSize - plusSize),
+                                         (int)plusSize);
+        await currentPartStream.WriteAsync(chunkSlice.ToArray(),
+                                           cancellationToken)
+                               .ConfigureAwait(false);
+        await remainingStream.WriteAsync(remainingSlice.ToArray(),
+                                         cancellationToken)
+                             .ConfigureAwait(false);
+      }
+
+      if (bytesRead >= currentPartSize && partNumber < MaxPartCount)
+      {
+        var partRequest = new UploadPartRequest
+                          {
+                            BucketName  = bucketName,
+                            Key         = objectKey,
+                            PartNumber  = partNumber,
+                            InputStream = new MemoryStream(currentPartStream.ToArray()),
+                            UploadId    = uploadId,
+                          };
+        yield return partRequest;
+        currentPartStream = new MemoryStream();
+        await remainingStream.CopyToAsync(currentPartStream,
+                                          cancellationToken)
+                             .ConfigureAwait(false);
+        bytesRead = remainingStream.Length;
+        partNumber++;
+        currentPartSize += 1024 * 1024; // 1MiB
+        if (currentPartSize > MaxPartSize)
+        {
+          currentPartSize = MaxPartSize;
+        }
+      }
+    }
+
+    /* This "may be" the last part, in one of these cases:
+     Either we have a remainder part of size < MinPartSize
+     Or we have reached the limit of the maximum number of parts and we still have data. We have already prepared MaxPartCount - 1 = 9999, the remaining data is accumulated and sent in the last(the 10000th) part, there is no guarantee here that it is < 5GiB
+    */
+    if (currentPartStream.Length >= 0)
+    {
+      var partRequest = new UploadPartRequest
+                        {
+                          BucketName  = bucketName,
+                          Key         = objectKey,
+                          PartNumber  = partNumber,
+                          InputStream = new MemoryStream(currentPartStream.ToArray()),
+                          UploadId    = uploadId,
+                        };
+      yield return partRequest;
     }
   }
 }
