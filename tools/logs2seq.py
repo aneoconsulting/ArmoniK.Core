8ab28fcb12ce5d2da39b306dee9e1f4ed0262bcc
jeremyzyngeraneo <jzynger@aneo.fr>
2023-09-05 17:50:13 +0200

    add: use Bytes type for tosend as log_message and Add logging module

--- tools/logs2seq.py
+++ tools/logs2seq.py
@@ -1,4 +1,5 @@
 import requests
+from pathlib import Path
 import zipfile
 import json
 import io
@@ -6,6 +7,8 @@ import argparse
 import boto3
 import os
 import gzip
+from typing import IO
+import logging
 
 # How to run seq in docker
 # docker rm -f seqlogpipe
@@ -24,16 +27,27 @@ import gzip
 #
 # export AWS_PROFILE=armonikDev
 
-parser = argparse.ArgumentParser(description="Download ArmoniK logs in JSON CLEF format from S3 bucket then send them to Seq.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+logger = logging.getLogger(Path(__file__).name)
+logging.basicConfig(
+    level=logging.INFO
+)
+
+parser = argparse.ArgumentParser(description="Download ArmoniK logs in JSON CLEF format from S3 bucket then send them to Seq.",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("bucket_name", help="S3 bucket", type=str)
-parser.add_argument("folder_name_core", help="Folder where core logs are located", type=str)
+parser.add_argument("folder_name_core",
+                    help="Folder where core logs are located", type=str)
 parser.add_argument("run_number", help="GitHub workflow run_number", type=str)
-parser.add_argument("run_attempt", help="GitHub workflow run_attempt", type=str)
-parser.add_argument("file_name", help="file to download from the bucket", type=str)
-parser.add_argument("--url", dest="url", help="Seq url", type=str, default="http://localhost:9341/api/events/raw?clef")
+parser.add_argument(
+    "run_attempt", help="GitHub workflow run_attempt", type=str)
+parser.add_argument(
+    "file_name", help="file to download from the bucket", type=str)
+parser.add_argument("--url", dest="url", help="Seq url", type=str,
+                    default="http://localhost:9341/api/events/raw?clef")
 args = parser.parse_args()
 
-dir_name = args.folder_name_core + "/" + args.run_number + "/" + args.run_attempt + "/"
+dir_name = args.folder_name_core + "/" + \
+    args.run_number + "/" + args.run_attempt + "/"
 tmp_dir = "./tmp/"
 obj_name = dir_name + args.file_name
 file_name = tmp_dir + obj_name
@@ -43,41 +57,42 @@ os.makedirs(tmp_dir + dir_name, exist_ok=True)
 s3 = boto3.client('s3')
 s3.download_file(args.bucket_name, obj_name, file_name)
 
-def process_json_log(url, file_name):
-    batch = 0
+
+def process_json_log(url, file_name: IO[bytes]):
     ctr = 0
-    tosend = ""
+    tosend = b""
     with open(file_name, "r") as file:
         for line in file.readlines():
             if line.startswith("{"):
-                tosend += line + "\n"
-            if batch > 100:
-                requests.post(url, data=tosend)
-                tosend = ""
-                batch = 0
-            batch = batch + 1
-            ctr = ctr + 1
-        print("sent :", ctr)
-        if tosend != "":
+                ctr = ctr + 1
+                log_message = bytes(line, "utf-8")
+                if len(tosend) + len(log_message) > 100000:
+                    requests.post(url, tosend)
+                    tosend = log_message
+                else:
+                    tosend += log_message
+        logger.info(f"sent : {ctr}")
+        if tosend != b"":
             requests.post(url, data=tosend)
 
-def process_jsongz_log(url, file_name):
-    batch = 0
+
+def process_jsongz_log(url, file_name: IO[bytes]):
     ctr = 0
-    tosend = ""
+    tosend = b""
     with gzip.open(file_name, "r") as file:
         for line in file.read().decode("utf-8").split("\n"):
             if line.startswith("{"):
-                tosend += line + "\n"
-            if batch > 100:
-                requests.post(url, data=tosend)
-                tosend = ""
-                batch = 0
-            batch = batch + 1
-            ctr = ctr + 1
-        print("sent :", ctr)
-        if tosend != "":
+                ctr = ctr + 1
+                log_message = bytes(line, "utf-8")
+                if len(tosend) + len(log_message) > 100000:
+                    requests.post(url, tosend)
+                    tosend = log_message
+                else:
+                    tosend += log_message
+        logger.info(f"sent : {ctr}")
+        if tosend != b"":
             requests.post(url, data=tosend)
+
 
 if file_name.endswith(".json.tar.gz"):
     process_jsongz_log(args.url, file_name)
