55276d6a568882dd273cbd8f184289768c3ddb1e
jeremyzyngeraneo <jzynger@aneo.fr>
2023-09-14 17:31:51 +0200

    refactor: Add class and methodes to the script

--- tools/logs2seq.py
+++ tools/logs2seq.py
@@ -34,73 +34,99 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-parser = argparse.ArgumentParser(description="Download ArmoniK logs in JSON CLEF format from S3 bucket then send them to Seq.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("bucket_name", help="S3 bucket", type=str)
-parser.add_argument("folder_name_core", help="Folder where core logs are located", type=str)
-parser.add_argument("run_number", help="GitHub workflow run_number", type=str)
-parser.add_argument("run_attempt", help="GitHub workflow run_attempt", type=str)
-parser.add_argument("file_name", help="file to download from the bucket", type=str)
-parser.add_argument("--url", dest="url", help="Seq url", type=str, default="http://localhost:9341/api/events/raw?clef")
-args = parser.parse_args()
+class LogSender:
+    def __init__(self, url: str, max_batch_size: int = 100000):
+        self.url = url
+        self.max_batch_size = max_batch_size
+        self.batch = b""
+        self.ctr = 0
 
-dir_name = args.folder_name_core + "/" + args.run_number + "/" + args.run_attempt + "/"
-tmp_dir = "./tmp/"
-obj_name = dir_name + args.file_name
-file_name = tmp_dir + obj_name
 
-os.makedirs(tmp_dir + dir_name, exist_ok=True)
+    def send_log(self, log_message: str):
+        """
+        Send a log message to a seq URL. logs are batched until the maximum batch size is reached (100000b),
+        and then they are sent
 
-s3 = boto3.client('s3')
-s3.download_file(args.bucket_name, obj_name, file_name)
+        Args:
+            log_message (str): The log message to send
 
-def process_json_log(url: str, file_name: str):
-    ctr = 0
-    tosend = b""
-    with open(file_name, "r") as file:
-        for line in file.readlines():
+        """
+        if len(self.batch) + len(log_message) > self.max_batch_size:
+            requests.post(self.url, data=self.batch)
+            self.batch = b""
+        self.batch += log_message
+        self.ctr += 1
+        logger.debug(f"sending : {len(self.batch)} bytes")
+
+
+    def read_file(self, file_name: str):
+        """
+        Read the content of a text file, Gzip supported
+
+        Args:
+            file_name (str): The name of the file to read
+
+        Returns:
+            str: The content of the read file
+
+        """
+        if file_name.endswith('.json.tar.gz'):
+            with gzip.open(file_name, 'rt', encoding="utf-8") as file:
+                content = file.read()
+        elif file_name.endswith('.json'):
+            with open(file_name, 'r', encoding="utf-8") as file:
+                content = file.read()
+        return content
+
+
+    def process_json_log(self, file_name: str):
+        """
+        Read a JSON log file, extract log messages, and send them to a Seq server.
+
+        Args:
+            file_name (str): The name of the file to read
+
+        """
+        json_data = self.read_file(file_name)
+        for line in json_data.split('\n'):
             if line.startswith("{"):
                 try:
                     parsed = json.loads(line)
                     if "@t" not in parsed:
                         continue
-                    ctr = ctr + 1
                     log_message = bytes(line + "\n", "utf-8")
-                    if len(tosend) + len(log_message) > 100000:
-                        requests.post(url, data=tosend)
-                        tosend = log_message
-                    else:
-                        tosend += log_message
+                    self.send_log(log_message)
                 except JSONDecodeError as e:
                     logger.warning(f"Failed to parse JSON: {e}")
-        logger.info(f"sent : {ctr}")
-        if tosend != b"":
-            requests.post(url, data=tosend)
+        if self.batch != b"":
+            requests.post(self.url, data=self.batch)
+        logger.info(f"sent: {self.ctr}")
 
 
-def process_jsongz_log(url: str, file_name: str):
-    ctr = 0
-    tosend = b""
-    with gzip.open(file_name, "r") as file:
-        for line in file.read().decode("utf-8").split("\n"):
-            if line.startswith("{"):
-                try:
-                    parsed = json.loads(line)
-                    if "@t" not in parsed:
-                        continue
-                    ctr = ctr + 1
-                    log_message = bytes(line + "\n", "utf-8")
-                    if len(tosend) + len(log_message) > 100000:
-                        requests.post(url, data=tosend)
-                        tosend = log_message
-                    else:
-                        tosend += log_message
-                except JSONDecodeError as e:
-                    logger.warning(f"Failed to parse JSON: {e}")
-        logger.info(f"sent : {ctr}")
-        if tosend != b"":
-            requests.post(url, data=tosend)
 
-if file_name.endswith(".json.tar.gz"):
-    process_jsongz_log(args.url, file_name)
-elif file_name.endswith(".json"):
-    process_json_log(args.url, file_name)
+def main():
+    parser = argparse.ArgumentParser(description="Download ArmoniK logs in JSON CLEF format from S3 bucket then send them to Seq.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("bucket_name", help="S3 bucket", type=str)
+    parser.add_argument("folder_name_core", help="Folder where core logs are located", type=str)
+    parser.add_argument("run_number", help="GitHub workflow run_number", type=str)
+    parser.add_argument("run_attempt", help="GitHub workflow run_attempt", type=str)
+    parser.add_argument("file_name", help="file to download from the bucket", type=str)
+    parser.add_argument("--url", dest="url", help="Seq url", type=str, default="http://localhost:9341/api/events/raw?clef")
+    args = parser.parse_args()
+
+    tmp_dir = "./tmp/"
+    dir_name = os.path.join(
+        args.folder_name_core, args.run_number, args.run_attempt)
+    obj_name = os.path.join(dir_name, args.file_name)
+    file_name = os.path.join(tmp_dir, obj_name)
+
+    os.makedirs(os.path.join(tmp_dir, dir_name), exist_ok=True)
+
+    s3 = boto3.client('s3')
+    s3.download_file(args.bucket_name, obj_name, file_name)
+
+    log_sender = LogSender(args.url)
+    log_sender.process_json_log(file_name)
+
+if __name__ == "__main__":
+    main()
