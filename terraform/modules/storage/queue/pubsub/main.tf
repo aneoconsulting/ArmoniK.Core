64b67bb03d54f0d9aef9ededaed28341e392ce21
Jérôme Gurhem <jgurhem@aneo.fr>
2024-02-05 18:40:56 +0100

    fix: wait for healtch check to be ok

--- terraform/modules/storage/queue/pubsub/main.tf
+++ terraform/modules/storage/queue/pubsub/main.tf
@@ -12,9 +12,18 @@ resource "docker_container" "queue" {
   }
 
   command = ["gcloud", "beta", "emulators", "pubsub", "start", "--project=plugincore", "--host-port=0.0.0.0:8085"]
+  wait    = true
 
   ports {
     internal = 8085
     external = var.exposed_ports.connection
+  }
+
+  healthcheck {
+    test         = concat(["CMD", "curl", "-fsSl", "localhost:8085"])
+    interval     = "10s"
+    timeout      = "3s"
+    start_period = "10s"
+    retries      = "10"
   }
 }
