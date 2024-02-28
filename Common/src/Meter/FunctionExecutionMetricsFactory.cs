3a9cf6eba744676b0b18a60619f18c700d6ae1a7
Jérôme Gurhem <jgurhem@aneo.fr>
2024-02-28 08:12:05 +0100

    fix: add missing file

---
+++ Common/src/Meter/FunctionExecutionMetricsFactory.cs
@@ -0,0 +1,52 @@
+// This file is part of the ArmoniK project
+// 
+// Copyright (C) ANEO, 2021-2024. All rights reserved.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+using ArmoniK.Core.Common.Pollster;
+
+using JetBrains.Annotations;
+
+namespace ArmoniK.Core.Common.Meter;
+
+[UsedImplicitly]
+public class FunctionExecutionMetricsFactory
+{
+  public const     string                           Name = $"ArmoniK.Core.{nameof(FunctionExecutionMetrics)}";
+  private readonly System.Diagnostics.Metrics.Meter meter_;
+  private readonly KeyValuePair<string, object?>[]  tags_;
+
+  public FunctionExecutionMetricsFactory(IMeterFactory meterFactory)
+  {
+    tags_ = new KeyValuePair<string, object?>[]
+            {
+              new($"{Name}.{nameof(AgentIdentifier.OwnerPodId)}".ToLower(),
+                  AgentIdentifier.OwnerPodId),
+              new($"{Name}.{nameof(AgentIdentifier.OwnerPodName)}".ToLower(),
+                  AgentIdentifier.OwnerPodName),
+            };
+    meter_ = meterFactory.Create(Name,
+                                 null,
+                                 tags_);
+  }
+
+  public FunctionExecutionMetrics Create(string name)
+    => new(meter_,
+           name,
+           tags_);
+}
