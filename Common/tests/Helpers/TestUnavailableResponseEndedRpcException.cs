4de88ee56f6228ff050c5951f4d789cb0606135a
Jérôme Gurhem <jgurhem@aneo.fr>
2024-04-16 10:21:26 +0200

    fix: worker crash during cancellation should be treated as error

---
+++ Common/tests/Helpers/TestUnavailableResponseEndedRpcException.cs
@@ -0,0 +1,33 @@
+// This file is part of the ArmoniK project
+// 
+// Copyright (C) ANEO, 2021-2024. All rights reserved.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Net.Http;
+
+using Grpc.Core;
+
+namespace ArmoniK.Core.Common.Tests.Helpers;
+
+public class TestUnavailableResponseEndedRpcException : RpcException
+{
+  public TestUnavailableResponseEndedRpcException()
+    : base(new Status(StatusCode.Unavailable,
+                      "Unavailable with response ended",
+                      new HttpRequestException("response ended",
+                                               new HttpIOException(HttpRequestError.ResponseEnded))))
+  {
+  }
+}
