2c6e5939c4c19dcfdb60adeada95d80ed3856d57
Jérôme Gurhem <jgurhem@aneo.fr>
2024-08-08 17:11:57 +0200

    style: format code

--- terraform/modules/monitoring/fluentbit/main.tf
+++ terraform/modules/monitoring/fluentbit/main.tf
@@ -13,17 +13,17 @@ resource "docker_container" "fluentbit" {
   }
 
   upload {
-    file = "/fluent-bit/etc/fluent-bit.conf"
+    file   = "/fluent-bit/etc/fluent-bit.conf"
     source = abspath("${path.root}/fluent-bit/etc/fluent-bit.conf")
   }
 
   upload {
-    file = "/fluent-bit/etc/parsers.conf"
+    file   = "/fluent-bit/etc/parsers.conf"
     source = abspath("${path.root}/fluent-bit/etc/parsers.conf")
   }
 
   upload {
-    file = "/fluent-bit/etc/append_time.lua"
+    file   = "/fluent-bit/etc/append_time.lua"
     source = abspath("${path.root}/fluent-bit/etc/append_time.lua")
   }
 
--- terraform/modules/storage/database/mongo/main.tf
+++ terraform/modules/storage/database/mongo/main.tf
@@ -23,24 +23,24 @@ resource "docker_container" "database" {
   dynamic "healthcheck" {
     for_each = var.mongodb_params.windows ? [] : [1]
     content {
-      test = [ "CMD", "mongosh", "--quiet", "--eval", "db.runCommand('ping').ok" ]
+      test     = ["CMD", "mongosh", "--quiet", "--eval", "db.runCommand('ping').ok"]
       interval = "3s"
-      retries = "2"
-      timeout = "2s"
+      retries  = "2"
+      timeout  = "2s"
     }
   }
 }
 
 resource "time_sleep" "wait" {
   create_duration = var.mongodb_params.windows ? "15s" : "0s"
-  depends_on = [docker_container.database]
+  depends_on      = [docker_container.database]
 }
 
 locals {
   linux_run = "docker run --net ${var.network} ${docker_image.database.image_id} mongosh mongodb://${docker_container.database.name}:27017/${var.mongodb_params.database_name}"
   // mongosh is not installed in windows docker images so we need it to be installed locally
   windows_run = "mongosh.exe mongodb://localhost:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name}"
-  prefix_run = var.mongodb_params.windows ? local.windows_run : local.linux_run
+  prefix_run  = var.mongodb_params.windows ? local.windows_run : local.linux_run
 }
 
 resource "null_resource" "init_replica" {
