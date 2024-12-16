a4e974a9b54bca50ee655c87a1b2dd0e36e22767
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2024-12-16 15:21:40 +0100

    fix: implem ping for mongo and change dns to ipv4 use

--- terraform/modules/storage/database/mongo/main.tf
+++ terraform/modules/storage/database/mongo/main.tf
@@ -41,23 +41,40 @@ resource "docker_container" "database" {
   }
 }
 resource "time_sleep" "wait" {
-  create_duration = var.mongodb_params.windows ? "15s" : "0s"
+  create_duration = var.mongodb_params.windows ? "30s" : "0s"
   depends_on      = [docker_container.database]
 }
 
 locals {
-  linux_run = "docker exec ${docker_container.database.name} mongosh mongodb://localhost:27017/${var.mongodb_params.database_name} --tls --tlsCAFile /mongo-certificate/ca.pem"
+  linux_run = "docker exec ${docker_container.database.name} mongosh mongodb://127.0.0.1:27017/${var.mongodb_params.database_name} --tls --tlsCAFile /mongo-certificate/ca.pem"
   // mongosh is not installed in windows docker images so we need it to be installed locally
-  windows_run = "mongosh.exe mongodb://localhost:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name} --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
+  windows_run = "mongosh.exe mongodb://127.0.0.1:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name} --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
   prefix_run  = var.mongodb_params.windows ? local.windows_run : local.linux_run
+}
+
+resource "null_resource" "test_connection" {
+  provisioner "local-exec" {
+    command = <<EOT
+      output=$(${local.prefix_run} --eval "db.runCommand('ping')"); 
+      echo "$output"; 
+      if echo "$output" | grep -q '"ok" : 1'; then 
+        echo "Ping successful"; 
+      else 
+        echo "Ping failed"; 
+        exit 1; 
+      fi
+    EOT
+  }
+  depends_on = [time_sleep.wait]
 }
 
 resource "null_resource" "init_replica" {
   provisioner "local-exec" {
-    command = "${local.prefix_run} --eval \"rs.initiate({_id: '${var.mongodb_params.replica_set_name}', members: [{_id: 0, host: 'localhost:27017'}]})\""
+    command = "${local.prefix_run} --eval \"rs.initiate({_id: '${var.mongodb_params.replica_set_name}', members: [{_id: 0, host: '127.0.0.1:27017'}]})\""
   }
-  depends_on = [time_sleep.wait]
+  depends_on = [null_resource.test_connection]
 }
+
 
 resource "null_resource" "partitions_in_db" {
   for_each = var.partition_list
