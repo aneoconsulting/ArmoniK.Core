4ef5bfddedbd8ad6c5f182e69e6e5e1a52f36014
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2024-12-16 16:43:16 +0100

    fix: syntax, removing more quotes for mongo init

--- terraform/modules/storage/database/mongo/main.tf
+++ terraform/modules/storage/database/mongo/main.tf
@@ -24,7 +24,7 @@ resource "docker_container" "database" {
     for_each = var.mongodb_params.windows ? [] : [1]
     content {
       test     = ["CMD", "mongosh", "--quiet", "--tls", "--tlsCAFile", "/mongo-certificate/ca.pem", "--eval", "db.runCommand('ping').ok"]
-     interval = "3s"
+      interval = "3s"
       retries  = "2"
       timeout  = "3s"
     }
@@ -54,8 +54,8 @@ locals {
 
 resource "null_resource" "init_replica" {
   provisioner "local-exec" {
-    command = "${local.prefix_run} --eval rs.initiate({_id: \\\"${var.mongodb_params.replica_set_name}\\\", members: [{_id: 0, host: \\\"127.0.0.1:27017\\\"}]})"
- }
+    command = "${local.prefix_run} --eval rs.initiate({_id: \"${var.mongodb_params.replica_set_name}\", members: [{_id: 0, host: \"127.0.0.1:27017\"}]})"
+  }
   depends_on = [time_sleep.wait]
 }
 
