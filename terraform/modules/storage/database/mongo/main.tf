758e39911741ae3a8547934e3be0dca1bc1a3483
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2024-12-16 11:36:36 +0100

    fix: change dns to init repset

--- terraform/modules/storage/database/mongo/main.tf
+++ terraform/modules/storage/database/mongo/main.tf
@@ -7,7 +7,7 @@ resource "docker_container" "database" {
   name  = var.mongodb_params.database_name
   image = docker_image.database.image_id
 
-  command = ["mongod", "--bind_ip_all", "--replSet", var.mongodb_params.replica_set_name]
+  command = ["mongod", "--bind_ip_all", "--replSet", var.mongodb_params.replica_set_name, "--tlsMode=requireTLS", "--tlsDisabledProtocols=TLS1_0", "--tlsCertificateKeyFile=/mongo-certificate/key.pem", "--tlsCAFile=/mongo-certificate/ca.pem", "--tlsAllowConnectionsWithoutCertificates"]
 
   networks_advanced {
     name = var.network
@@ -23,38 +23,38 @@ resource "docker_container" "database" {
   dynamic "healthcheck" {
     for_each = var.mongodb_params.windows ? [] : [1]
     content {
-      test     = ["CMD", "mongosh", "--quiet", "--eval", "db.runCommand('ping').ok"]
-      interval = "3s"
+      test     = ["CMD", "mongosh", "--quiet", "--tls", "--tlsCAFile", "/mongo-certificate/ca.pem", "--eval", "db.runCommand('ping').ok"]
+     interval = "3s"
       retries  = "2"
-      timeout  = "2s"
+      timeout  = "3s"
     }
   }
-}
 
+  upload {
+    file    = "/mongo-certificate/key.pem"
+    content = local.server_key
+  }
+
+  upload {
+    file    = "/mongo-certificate/ca.pem"
+    content = tls_locally_signed_cert.mongodb_certificate.ca_cert_pem
+  }
+}
 resource "time_sleep" "wait" {
   create_duration = var.mongodb_params.windows ? "15s" : "0s"
   depends_on      = [docker_container.database]
 }
 
 locals {
-  linux_run = "docker run --net ${var.network} ${docker_image.database.image_id} mongosh mongodb://${docker_container.database.name}:27017/${var.mongodb_params.database_name}"
+  linux_run = "docker exec ${docker_container.database.name} mongosh mongodb://127.0.0.1:27017/${var.mongodb_params.database_name} --tls --tlsCAFile /mongo-certificate/ca.pem"
   // mongosh is not installed in windows docker images so we need it to be installed locally
-  windows_run = "mongosh.exe mongodb://localhost:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name}"
+  windows_run = "mongosh.exe mongodb://127.0.0.1:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name} --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
   prefix_run  = var.mongodb_params.windows ? local.windows_run : local.linux_run
 }
 
-output "init_replica_command" {
-  value = var.mongodb_params.windows ? 
-    "powershell -Command \"${local.windows_run} --eval \\\"rs.initiate({_id: '${var.mongodb_params.replica_set_name}', members: [{_id: 0, host: '127.0.0.1:27017'}]})\\\"\"" :
-    "${local.linux_run} --eval 'rs.initiate({_id: \"${var.mongodb_params.replica_set_name}\", members: [{_id: 0, host: \"127.0.0.1:27017\"}]})'"
-}
-
-
 resource "null_resource" "init_replica" {
   provisioner "local-exec" {
-    command = var.mongodb_params.windows ? 
-      "powershell -Command \"${local.windows_run} --eval \\\"rs.initiate({_id: '${var.mongodb_params.replica_set_name}', members: [{_id: 0, host: '127.0.0.1:27017'}]})\\\"\"" :
-      "${local.linux_run} --eval 'rs.initiate({_id: \"${var.mongodb_params.replica_set_name}\", members: [{_id: 0, host: \"127.0.0.1:27017\"}]})'"
+    command = "${local.prefix_run} --eval \"rs.initiate({_id: '${var.mongodb_params.replica_set_name}', members: [{_id: 0, host: '127.0.0.1:27017'}]})\""
   }
   depends_on = [time_sleep.wait]
 }
@@ -62,9 +62,7 @@ resource "null_resource" "init_replica" {
 resource "null_resource" "partitions_in_db" {
   for_each = var.partition_list
   provisioner "local-exec" {
-    command = var.mongodb_params.windows ?
-      "powershell -Command \"${local.windows_run} --eval \\\"db.PartitionData.insertOne(${jsonencode(each.value)})\\\"\"" :
-      "${local.linux_run} --eval 'db.PartitionData.insertOne(${jsonencode(each.value)})'"
+    command = "${local.prefix_run} --eval 'db.PartitionData.insertOne(${jsonencode(each.value)})'"
   }
   depends_on = [null_resource.init_replica]
 }
