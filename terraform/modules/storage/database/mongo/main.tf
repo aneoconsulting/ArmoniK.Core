b3f03e7b4c65be29c4fdd66520fb56961b08681e
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2024-12-17 12:12:38 +0100

    fix: implem condition for linux/windows and correction on initrepset for tls

--- terraform/modules/storage/database/mongo/main.tf
+++ terraform/modules/storage/database/mongo/main.tf
@@ -24,7 +24,7 @@ resource "docker_container" "database" {
     for_each = var.mongodb_params.windows ? [] : [1]
     content {
       test     = ["CMD", "mongosh", "--quiet", "--tls", "--tlsCAFile", "/mongo-certificate/ca.pem", "--eval", "db.runCommand('ping').ok"]
-     interval = "3s"
+      interval = "3s"
       retries  = "2"
       timeout  = "3s"
     }
@@ -34,6 +34,10 @@ resource "docker_container" "database" {
     file    = "/mongo-certificate/key.pem"
     content = local.server_key
   }
+  upload {
+    file    = "/mongo-init.js"
+    content = local.mongo_init_repset
+  }
 
   upload {
     file    = "/mongo-certificate/ca.pem"
@@ -41,23 +45,35 @@ resource "docker_container" "database" {
   }
 }
 resource "time_sleep" "wait" {
-  create_duration = var.mongodb_params.windows ? "15s" : "0s"
+  create_duration = var.mongodb_params.windows ? "30s" : "0s"
   depends_on      = [docker_container.database]
 }
-
 locals {
-  linux_run = "docker exec ${docker_container.database.name} mongosh mongodb://localhost:27017/${var.mongodb_params.database_name} --tls --tlsCAFile /mongo-certificate/ca.pem"
+  linux_run = "docker exec ${docker_container.database.name} mongosh mongodb://127.0.0.1:27017/${var.mongodb_params.database_name} --tls --tlsCAFile /mongo-certificate/ca.pem"
   // mongosh is not installed in windows docker images so we need it to be installed locally
-  windows_run = "mongosh.exe mongodb://localhost:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name} --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
-  prefix_run  = var.mongodb_params.windows ? local.windows_run : local.linux_run
+  windows_run       = "mongosh.exe mongodb://127.0.0.1:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name} --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
+  prefix_run        = var.mongodb_params.windows ? local.windows_run : local.linux_run
+  mongo_init_repset = <<EOT
+rs.initiate({
+  _id: "${var.mongodb_params.replica_set_name}",
+  members: [
+    {_id: 0, host: "127.0.0.1:27017"}
+  ]
+});
+EOT
 }
+resource "local_file" "mongo_init" {
+  content  = local.mongo_init_repset
+  filename = "${path.root}/generated/mongo/mongo_init.js"
 
+}
 resource "null_resource" "init_replica" {
   provisioner "local-exec" {
-    command = "${local.prefix_run} --eval \"rs.initiate({_id: '${var.mongodb_params.replica_set_name}', members: [{_id: 0, host: 'localhost:27017'}]})\""
+    command = var.mongodb_params.windows ? "${local.windows_run} --file ${local_file.mongo_init.filename}" : "${local.linux_run} --file /mongo-init.js"
   }
   depends_on = [time_sleep.wait]
 }
+
 
 resource "null_resource" "partitions_in_db" {
   for_each = var.partition_list
