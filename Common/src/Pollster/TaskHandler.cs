a2e1c7341a50fa3bb0a8b816703c995488890f5e
italo1aguiar-aneo <iaguiar@aneo.fr>
2025-01-29 11:04:14 +0100

    fix: resubmitted task due to pod crash is never processed

--- Common/src/Pollster/TaskHandler.cs
+++ Common/src/Pollster/TaskHandler.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2025. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -678,6 +678,12 @@ public sealed class TaskHandler : IAsyncDisposable
                                        .ConfigureAwait(false);
             return AcquisitionStatus.AcquisitionFailedTaskCancelling;
           }
+        }
+
+        if (taskData_.Status is TaskStatus.Submitted)
+        {
+          messageHandler_.Status = QueueMessageStatus.Processed;
+          return AcquisitionStatus.TaskSubmittedWithNoPossibleExecution;
         }
 
         // if the task is running elsewhere, then the message is duplicated so we remove it from the queue
