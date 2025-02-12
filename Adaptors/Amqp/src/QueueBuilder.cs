03026c298d8acc41e15c2ae5a27a0470ae8efad4
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2025-02-12 14:21:51 +0100

    refactor: remove unused namespaces

--- Adaptors/Amqp/src/QueueBuilder.cs
+++ Adaptors/Amqp/src/QueueBuilder.cs
@@ -15,9 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-using System.Security.Cryptography.X509Certificates;
-
 using ArmoniK.Core.Base;
 using ArmoniK.Core.Utils;
 
