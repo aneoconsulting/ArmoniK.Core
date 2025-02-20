cc06c2a70a62b5be092f22e28c091b1d32f6d40e
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2025-02-20 16:47:33 +0100

    refactor: centralize environment variable management for AMQP and RabbitMQ adapters

--- terraform/modules/storage/queue/activemq/outputs.tf
+++ terraform/modules/storage/queue/activemq/outputs.tf
@@ -1,15 +1,3 @@
 output "generated_env_vars" {
-  value = ({
-    "Components__QueueAdaptorSettings__ClassName"           = "ArmoniK.Core.Adapters.Amqp.QueueBuilder"
-    "Components__QueueAdaptorSettings__AdapterAbsolutePath" = "/adapters/queue/amqp/ArmoniK.Core.Adapters.Amqp.dll"
-    "Amqp__User"                                            = "${var.queue_envs.user}"
-    "Amqp__Password"                                        = "${var.queue_envs.password}"
-    "Amqp__Host"                                            = "${var.queue_envs.host}"
-    "Amqp__Port"                                            = "${var.queue_envs.port}"
-    "Amqp__Scheme"                                          = "AMQP"
-    "Amqp__PartitionId"                                     = "TestPartition"
-    "Amqp__MaxPriority"                                     = "${var.queue_envs.max_priority}"
-    "Amqp__MaxRetries"                                      = "${var.queue_envs.max_retries}"
-    "Amqp__LinkCredit"                                      = "${var.queue_envs.link_credit}"
-  })
+  value = local.generated_env_vars
 }
