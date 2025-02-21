b6dca636d1a19ddf853eb628d11d74e7316ecbb5
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2025-02-21 11:59:29 +0100

    refactor: remove commented code and spaces

--- terraform/modules/storage/queue/rabbitmq/main.tf
+++ terraform/modules/storage/queue/rabbitmq/main.tf
@@ -10,20 +10,6 @@ resource "docker_container" "queue" {
   networks_advanced {
     name = var.network
   }
-  # env = [
-  #   "Components__QueueAdaptorSettings__ClassName=${local.generated_env_vars.Components__QueueAdaptorSettings__ClassName}",
-  #   "Components__QueueAdaptorSettings__AdapterAbsolutePath=${path.root}/Adaptors/RabbitMQ/src/bin/Debug/net8.0/ArmoniK.Core.Adapters.RabbitMQ.dll",
-  #   "Amqp__User=${local.generated_env_vars.Amqp__User}",
-  #   "Amqp__Password=${local.generated_env_vars.Amqp__Password}",
-  #   "Amqp__Host=${local.generated_env_vars.Amqp__Host}",
-  #   "Amqp__Port=${local.generated_env_vars.Amqp__Port}",
-  #   "Amqp__Scheme=${local.generated_env_vars.Amqp__Scheme}",
-  #   "Amqp__PartitionId=${local.generated_env_vars.Amqp__PartitionId}",
-  #   "Amqp__MaxPriority=${local.generated_env_vars.Amqp__MaxPriority}",
-  #   "Amqp__MaxRetries=${local.generated_env_vars.Amqp__MaxRetries}",
-  #   "Amqp__LinkCredit=${local.generated_env_vars.Amqp__LinkCredit}",
-  #   "Amqp__AllowHostMismatch=false"
-  # ]
   env = local.generated_env
   ports {
     internal = 5672
