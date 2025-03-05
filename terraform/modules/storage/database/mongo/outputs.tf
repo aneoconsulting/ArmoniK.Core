5306757177a93a7a7dd1ceb2eba8743ced420542
Florian Lemaitre <flemaitre@aneo.fr>
2025-03-05 14:48:39 +0100

    Adapt deployment to use the ConnectionString option

--- terraform/modules/storage/database/mongo/outputs.tf
+++ terraform/modules/storage/database/mongo/outputs.tf
@@ -1,15 +1,22 @@
+locals {
+  query_parameters = compact([
+    var.mongodb_params.use_direct_connection ? "directConnection=true" : null,
+    can(coalesce(var.mongodb_params.replica_set_name)) ? "replicaSet=${var.mongodb_params.replica_set_name}" : null,
+    "tls=true",
+    "tlsInsecure=true",
+  ])
+
+  query_suffix = length(local.query_parameters) > 0 ? "?${join("&", local.query_parameters)}" : ""
+
+  connection_string = "mongodb://${docker_container.database.name}:${var.mongodb_params.exposed_port}/${docker_container.database.name}${local.query_suffix}"
+}
+
 output "generated_env_vars" {
   value = {
     "Components__TableStorage"               = "ArmoniK.Adapters.MongoDB.TableStorage"
-    "MongoDB__Host"                          = docker_container.database.name
-    "MongoDB__Port"                          = "${var.mongodb_params.exposed_port}"
-    "MongoDB__DatabaseName"                  = docker_container.database.name
-    "MongoDB__MaxConnectionPoolSize"         = "${var.mongodb_params.max_connection_pool_size}"
+    "MongoDB__ConnectionString"              = local.connection_string
     "MongoDB__TableStorage__PollingDelayMin" = "${var.mongodb_params.min_polling_delay}"
     "MongoDB__TableStorage__PollingDelayMax" = "${var.mongodb_params.max_polling_delay}"
-    "MongoDB__DirectConnection"              = "${var.mongodb_params.use_direct_connection}"
-    "MongoDB__ReplicaSet"                    = "${var.mongodb_params.replica_set_name}"
-    "MongoDB__Tls"                           = "true"
     "MongoDB__AllowInsecureTls"              = "true"
     "MongoDB__CAFile"                        = "/mongo-certificate/ca.pem"
     "MongoDB__ServerSelectionTimeout"        = "00:00:20"
