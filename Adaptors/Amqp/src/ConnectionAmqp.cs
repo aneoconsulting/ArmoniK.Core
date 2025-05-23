38156c6fb5ec02bcabf7012c58532a6173799be0
AncientPatata <110251221+AncientPatata@users.noreply.github.com>
2025-05-23 10:12:41 +0200

    Removed annoying try catch that obfuscated connection error

--- Adaptors/Amqp/src/ConnectionAmqp.cs
+++ Adaptors/Amqp/src/ConnectionAmqp.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2025. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -131,27 +131,15 @@ public class ConnectionAmqp : IConnectionAmqp
     var retry = 0;
     for (; retry < options.MaxRetries; retry++)
     {
-      try
-      {
-        var connection = await connectionFactory.CreateAsync(address)
-                                                .ConfigureAwait(false);
-        connection.AddClosedCallback((_,
-                                      e) => OnCloseConnection(e,
-                                                              logger));
+      var connection = await connectionFactory.CreateAsync(address)
+                                              .ConfigureAwait(false);
+      connection.AddClosedCallback((_,
+                                    e) => OnCloseConnection(e,
+                                                            logger));
 
-        return connection;
-      }
-      catch (Exception ex)
-      {
-        logger.LogInformation(ex,
-                              "Retrying to create connection");
-        await Task.Delay(1000 * retry,
-                         cancellationToken)
-                  .ConfigureAwait(false);
-      }
+      return connection;
     }
-
-    throw new TimeoutException($"{nameof(options.MaxRetries)} reached");
+    return null;
   }
 
   private static void OnCloseConnection(Error?  error,
