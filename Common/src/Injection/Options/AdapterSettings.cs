2820520dbdbffcb7c6202d7deae80d390d6b9826
José Fonseca <jfonseca@aneo.fr>
2025-06-27 16:28:30 +0200

    apply format patch

--- Common/src/Injection/Options/AdapterSettings.cs
+++ Common/src/Injection/Options/AdapterSettings.cs
@@ -1,24 +1,24 @@
 // This file is part of the ArmoniK project
-//
+// 
 // Copyright (C) ANEO, 2021-2025. All rights reserved.
-//
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace ArmoniK.Core.Common.Injection.Options;
 
 /// <summary>
-/// Represents configuration settings for an adapter.
+///   Represents configuration settings for an adapter.
 /// </summary>
 public class AdapterSettings
 {
@@ -29,7 +29,7 @@ public class AdapterSettings
 
   /// <summary>
   ///   Fully qualified name of the adapter class that implements the
-  ///   <see cref="ArmoniK.Core.Base.IDependencyInjectionBuildable"/> interface
+  ///   <see cref="ArmoniK.Core.Base.IDependencyInjectionBuildable" /> interface
   /// </summary>
   public string ClassName { get; set; } = "";
 }
--- Common/src/Injection/Options/InitWorker.cs
+++ Common/src/Injection/Options/InitWorker.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2025. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -32,10 +32,10 @@ public class InitWorker
   /// <summary>
   ///   Number of times the worker should retry a failed check before giving up.
   /// </summary>
-  public int      WorkerCheckRetries { get; set; } = 10;
+  public int WorkerCheckRetries { get; set; } = 10;
 
   /// <summary>
   ///   Delay duration between each retry attempt.
   /// </summary>
-  public TimeSpan WorkerCheckDelay   { get; set; } = TimeSpan.FromSeconds(2);
+  public TimeSpan WorkerCheckDelay { get; set; } = TimeSpan.FromSeconds(2);
 }
