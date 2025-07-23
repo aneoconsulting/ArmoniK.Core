5daed0fb884312af3d0071abd723c1fc388f6a76
ereali-aneo <ereali@aneo.fr>
2025-07-23 17:49:52 +0200

    chore: embed replica set and partition setup into init script

--- terraform/modules/storage/database/mongo/main.tf
+++ terraform/modules/storage/database/mongo/main.tf
@@ -51,16 +51,12 @@ resource "time_sleep" "wait" {
 locals {
   linux_run = "docker exec ${docker_container.database.name} mongosh mongodb://127.0.0.1:27017/${var.mongodb_params.database_name} --tls --tlsCAFile /mongo-certificate/ca.pem"
   // mongosh is not installed in windows docker images so we need it to be installed locally
-  windows_run       = "mongosh.exe mongodb://127.0.0.1:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name}?serverSelectionTimeoutMS=10000 --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
-  prefix_run        = var.mongodb_params.windows ? local.windows_run : local.linux_run
-  mongo_init_repset = <<EOT
-rs.initiate({
-  _id: "${var.mongodb_params.replica_set_name}",
-  members: [
-    {_id: 0, host: "127.0.0.1:27017"}
-  ]
-});
-EOT
+  windows_run = "mongosh.exe mongodb://127.0.0.1:${var.mongodb_params.exposed_port}/${var.mongodb_params.database_name}?serverSelectionTimeoutMS=10000 --tls --tlsCAFile ${local_sensitive_file.ca.filename}"
+  prefix_run  = var.mongodb_params.windows ? local.windows_run : local.linux_run
+  mongo_init_repset = templatefile("${path.module}/mongo_init.tpl.js", {
+    replica_set_name = var.mongodb_params.replica_set_name
+    partition_data   = jsonencode(values(var.partition_list))
+  })
 }
 resource "local_file" "mongo_init" {
   content  = local.mongo_init_repset
@@ -72,13 +68,4 @@ resource "null_resource" "init_replica" {
     command = var.mongodb_params.windows ? "${local.windows_run} --file ${local_file.mongo_init.filename}" : "${local.linux_run} --file /mongo-init.js"
   }
   depends_on = [time_sleep.wait]
-}
-
-
-resource "null_resource" "partitions_in_db" {
-  for_each = var.partition_list
-  provisioner "local-exec" {
-    command = "${local.prefix_run} --eval 'db.PartitionData.insertOne(${jsonencode(each.value)})'"
-  }
-  depends_on = [null_resource.init_replica]
 }
---
+++ terraform/modules/storage/database/mongo/mongo_init.tpl.js
@@ -0,0 +1,22 @@
+rs.initiate(
+{
+  _id: "${replica_set_name}",
+  members: [
+    {_id: 0, host: "127.0.0.1:27017"}
+  ]
+});
+
+let initialized = false;
+while(initialized === false)
+{
+  try {
+    if (rs.status().myState === 1) {
+      initialized = true;
+      sleep(10000);
+    }
+  } catch (e) {
+    sleep(1000);
+  }
+}
+
+db.PartitionData.insertMany(${partition_data});
