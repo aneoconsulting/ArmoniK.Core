0df69c819937f29e11d9817f296a75fdd5e60804
italo1aguiar-aneo <iaguiar@aneo.fr>
2025-09-29 16:50:15 +0200

    fix: delete subtasks from task that crashed while processing
    - Delete subtasks instead from task that crashed while processing
    - Purge fix implementation

--- Common/tests/TaskLifeCycleHelperTest.cs
+++ Common/tests/TaskLifeCycleHelperTest.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2025. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -1486,20 +1486,22 @@ public class TaskLifeCycleHelperTest
 
                       Assert.That(taskB,
                                   crashState >= CrashState.TasksCreated || !subtask
-                                    ? Has.ItemAt(0)
-                                         .Property("Status")
-                                         .EqualTo(committed || !subtask
-                                                    ? TaskStatus.Pending
-                                                    : TaskStatus.Cancelled)
+                                    ? committed || !subtask
+                                        ? Has.ItemAt(0)
+                                             .Property("Status")
+                                             .EqualTo(TaskStatus.Pending)
+                                        : Is.Empty // subtasks are deleted when not committed
                                     : Is.Empty);
 
                       Assert.That(outputB,
                                   crashState >= CrashState.ResultsCreated || !subtask
-                                    ? Has.ItemAt(0)
-                                         .Property("Status")
-                                         .EqualTo(committed || !subtask
-                                                    ? ResultStatus.Created
-                                                    : ResultStatus.Aborted)
+                                    ? committed || !subtask
+                                        ? Has.ItemAt(0)
+                                             .Property("Status")
+                                             .EqualTo(ResultStatus.Created)
+                                        : Has.ItemAt(0) // results are aborted when not committed
+                                             .Property("Status")
+                                             .EqualTo(ResultStatus.Aborted)
                                     : Is.Empty);
 
                       Assert.That(outputRoot.Status,
