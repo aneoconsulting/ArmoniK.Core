5ab03d603a43501bfeeecb62075bdac3da232830
José Fonseca <jfonseca@aneo.fr>
2025-10-01 14:37:58 +0200

    update utility version

--- tools/generate-envvars-doc.sh
+++ tools/generate-envvars-doc.sh
@@ -5,7 +5,7 @@ set -e
 SOLUTION_FILE=$(realpath "ArmoniK.Core.sln")
 OUTPUT_DIR=.docs/content/envars/
 
-dotnet tool install -g ArmoniK.Utils.DocExtractor --version 0.6.2-jfdocextractor.15.sha.ed3515f
+dotnet tool install -g ArmoniK.Utils.DocExtractor --version 0.6.2-jfflatten.25.sha.c6b2fbf
 
 cd $OUTPUT_DIR
 armonik.utils.docextractor -s $SOLUTION_FILE
