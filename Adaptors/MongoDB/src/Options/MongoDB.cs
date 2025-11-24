5fba0779674b1965b2c094af3e063fbf306583bb
italo1aguiar-aneo <iaguiar@aneo.fr>
2025-11-24 11:40:03 +0100

    feat: use mongo indexes optionally

--- Adaptors/MongoDB/src/Options/MongoDB.cs
+++ Adaptors/MongoDB/src/Options/MongoDB.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2025. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -65,4 +65,9 @@ public class MongoDB
   public bool Sharding { get; set; }
 
   public string AuthSource { get; set; } = "";
+
+  /// <summary>
+  ///   Indicates whether to use minimal indexes for the collections.
+  /// </summary>
+  public bool UseMinimalIndexes { get; set; } = false;
 }
