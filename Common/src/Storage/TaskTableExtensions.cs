30eb486c5bd9e20ad9b82ff60f380dd6109bc4fb
Jérôme Gurhem <jgurhem@aneo.fr>
2025-12-02 14:38:38 +0100

    fix: use list instead of array because it crashes Mongo driver

--- Common/src/Storage/TaskTableExtensions.cs
+++ Common/src/Storage/TaskTableExtensions.cs
@@ -37,14 +37,14 @@ namespace ArmoniK.Core.Common.Storage;
 /// </summary>
 public static class TaskTableExtensions
 {
-  private static readonly TaskStatus[] FinalStatus =
-  {
+  private static readonly List<TaskStatus> FinalStatus =
+  [
     TaskStatus.Completed,
     TaskStatus.Cancelled,
     TaskStatus.Error,
     TaskStatus.Retried,
     TaskStatus.Timeout,
-  };
+  ];
 
   private static readonly Expression<Func<TaskData, TaskData>> Identity = data => data;
 
