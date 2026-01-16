cfcdee37dc510be39251942d197e1b1a33e68b44
PierrePicard20 <pierre.picard20@gmail.com>
2026-01-16 11:29:16 +0100

    fix: ImportResultsData must preserve order of results

--- Utils/src/ByteArrayComparer.cs
+++ Utils/src/ByteArrayComparer.cs
@@ -49,7 +49,15 @@ public class ByteArrayComparer : IEqualityComparer<byte[]>
       return false;
     }
 
-    return x.SequenceEqual(y);
+    for (var i = 0; i < x.Length; i++)
+    {
+      if (x[i] != y[i])
+      {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   /// <summary>
