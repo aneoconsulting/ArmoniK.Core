ac9dda07a6f61d62bf9ebd3446db1c7e3ab82d2c
Jérôme Gurhem <jgurhem@aneo.fr>
2026-02-16 10:24:37 +0100

    require version => 3.9.0 for Docker provider

--- terraform/modules/ingress/versions.tf
+++ terraform/modules/ingress/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "registry.opentofu.org/kreuzwerker/docker"
-      version = ">= 3.0.2"
+      version = ">= 3.9.0"
     }
     pkcs12 = {
       source  = "chilicat/pkcs12"
