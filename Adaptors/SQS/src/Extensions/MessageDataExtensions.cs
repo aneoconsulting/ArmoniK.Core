d537443594c666c99fe4521aa401fbdc8d1bfecc
idhir.madjour <idhir.madjour@ca-cib.com>
2026-02-26 12:10:20 +0100

    feat: Use SessionId as GroupId on SQS
    - Fill SQS message groupId with sessionId.
    - Create an extension Method to transform messageData into SendBatchRequest.
    - Add a property within SQS
    - Move SQS into Options to have same logic everywhere.
    - Put taskId within the body.

--- Adaptors/SQS/src/Extensions/MessageDataExtensions.cs
+++ Adaptors/SQS/src/Extensions/MessageDataExtensions.cs
@@ -1,22 +1,21 @@
 ﻿// This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2026. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.Text.Json;
 
 using Amazon.SQS.Model;
 
@@ -33,7 +32,7 @@ internal static class MessageDataExtensions
                                        {
                                          Id = Guid.NewGuid()
                                                   .ToString(),
-                                         MessageBody = JsonSerializer.Serialize(messageData),
+                                         MessageBody = messageData.TaskId,
                                        };
 
     if (sqsOptions.UseSessionMessageGroupId)
