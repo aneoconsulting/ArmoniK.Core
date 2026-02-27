a90f2bbec543fb01f9f96f5a5fca5a044cae8d0a
Tenro <idhir.madjour@protonmail.com>
2026-02-27 09:40:25 +0100

    fix: Order SQS message push and pull code
    - Arrange the code to avoid unecessary OrderbyDesc.
    - Addition of the unit test for the pushing data

--- Common/src/Storage/TaskLifeCycleHelper.cs
+++ Common/src/Storage/TaskLifeCycleHelper.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2026. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -516,9 +516,10 @@ public static class TaskLifeCycleHelper
       return;
     }
 
-    if (sessionData.Status != SessionStatus.Paused)
+    if (sessionData.Status is not SessionStatus.Paused)
     {
-      foreach (var group in messages.GroupBy(msg => (msg.Options.PartitionId, msg.Options.Priority)))
+      var groupedMessageByPartitionAndOrderedByPriority = GroupMessageByPartitionAndOrderItByPriority(messages);
+      foreach (var group in groupedMessageByPartitionAndOrderedByPriority)
       {
         await pushQueueStorage.PushMessagesAsync(group,
                                                  group.Key.PartitionId,
@@ -535,6 +536,11 @@ public static class TaskLifeCycleHelper
                                          sessionData.Status == SessionStatus.Paused,
                                          cancellationToken)
                    .ConfigureAwait(false);
+  }
+
+  internal static IEnumerable<IGrouping<(string PartitionId, int PriorityId), MessageData>> GroupMessageByPartitionAndOrderItByPriority(IEnumerable<MessageData> dataMessages)
+  {
+    return dataMessages.GroupBy(msg => (msg.Options.PartitionId, msg.Options.Priority));
   }
 
   /// <summary>
