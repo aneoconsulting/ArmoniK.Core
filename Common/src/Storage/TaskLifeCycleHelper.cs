f38ac57adef0d7f0ef3cd7229e4d9c06fe0fb46e
idhir.madjour <idhir.madjour@ca-cib.com>
2026-03-19 11:50:37 +0100

    fix: Order sqs messages
    - Arrange the code to avoid unecessary OrderbyDesc.
    - Performing the code to have an order descending by Priority when pushing messages.
    - Remove fat finguer.

--- Common/src/Storage/TaskLifeCycleHelper.cs
+++ Common/src/Storage/TaskLifeCycleHelper.cs
@@ -1,17 +1,17 @@
 // This file is part of the ArmoniK project
-//
+// 
 // Copyright (C) ANEO, 2021-2026. All rights reserved.
-//
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,17 +21,8 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-using ArmoniK.Api.Common.Utils;
-using ArmoniK.Core.Base;
-using ArmoniK.Core.Base.DataStructures;
-using ArmoniK.Core.Base.Exceptions;
 using ArmoniK.Core.Common.Exceptions;
 using ArmoniK.Core.Common.Injection.Options;
-using ArmoniK.Utils;
-
-using Grpc.Core;
-
-using Microsoft.Extensions.Logging;
 
 namespace ArmoniK.Core.Common.Storage;
 
