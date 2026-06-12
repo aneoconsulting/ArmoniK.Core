1464872b61c1d315866c59f3acd5e3061fd52c3a
José Fonseca <jfonseca@aneo.fr>
2026-06-12 13:54:30 +0200

    chore(nats): use class decorator so document extraction tool picks nats options

--- Adaptors/Nats/src/Nats.cs
+++ Adaptors/Nats/src/Nats.cs
@@ -1,19 +1,21 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2026. All rights reserved.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using ArmoniK.Utils.DocAttribute;
 
 namespace ArmoniK.Core.Adapters.Nats;
 
@@ -21,6 +23,7 @@ namespace ArmoniK.Core.Adapters.Nats;
 ///   All Allowed option for Nats queue in ArmoniK.
 ///   Can be set through configuration sources.
 /// </summary>
+[ExtractDocumentation("Options for Nats")]
 internal class Nats
 {
   /// <summary>
